e3a128e6adb57d490f31c891b520fbadd7956489
popov <popich2005@mail.ru>
2024-04-26 14:17:32 +0300

    Initial Commit

---
+++ GameDartsSliderSUI/GameDartsSliderSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDartsSliderSUIApp.swift
+//  GameDartsSliderSUI
+//
+//  Created by Алексей Попов on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameDartsSliderSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
